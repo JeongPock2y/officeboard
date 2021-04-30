abe751a8987d5214667c800692ea6afbb4395643
younwook <rosopop@naver.com>
2021-04-30 15:40:37 +0900

    first  create Officeboard  with  Mustache

---
+++ officeboard/src/main/java/com/acma/officeboard/OfficeboardApplication.java
@@ -0,0 +1,13 @@
+package com.acma.officeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeboardApplication.class, args);
+	}
+
+}
---
+++ officeboard/src/test/java/com/acma/officeboard/OfficeboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acma.officeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
